0b90d22faf6d66f058867ed97482a767e9325c86
LeoDJ <leandro.spaeth@gmail.com>
2023-02-03 18:14:20 +0100

    use system encoding

--- P2Pro/util.py
+++ P2Pro/util.py
@@ -1,11 +1,12 @@
 import threading
+import sys
 
 class PipeLogger:
     @staticmethod
     def _proxy_lines(pipe, handler):
         with pipe:
             for line in pipe:
-                handler(line.decode('utf-8').rstrip())
+                handler(line.decode(sys.stdout.encoding).rstrip())
 
     def __init__(self, pipe, handler):
         threading.Thread(target=self._proxy_lines, args=[pipe, handler]).start()
